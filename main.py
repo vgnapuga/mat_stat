94243f010e790959c9704acc0d4c08d113deb50f
OneNull <shavrin_ns@mail.ru>
2025-03-26 18:31:57 +0300

    solution realisation for m = 1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def permutations(elements:list, n:int):
+    """Алгоритм Хипа"""
     yield elements.copy()
 
     c:list = [0] * n
@@ -23,16 +24,32 @@ def calculate_permutations(n:int) -> list:
     days:list = [day for day in range(1, n + 1)]
 
     all_permutations:list = list(permutations(days, n))
-    return print(all_permutations)
+    return all_permutations
 
-def solution(n:int, m:int) -> int:
+def solution(n:int, m:int) -> float:
     permutations:list = calculate_permutations(n)
+    result:int = 0
+
+    for permutation in permutations:
+        if (n in permutation[:m]):
+            continue
+
+        i = m
+        while (permutation[m - 1] < permutation[i]):
+            if (permutation[i] == n):
+                result += 1
+                break
+
+            i += 1
+
+    return result / len(permutations)
 
 def main() -> None:
     n:int = int(input("Введите общее кол-во дней: "))
     m:int = int(input("Введите кол-во дней для обязательного ожидания: "))
 
-    solution(n, m)
+    result:float = solution(n, m)
+    print(f"Вероятность продажи по максимальной цене: {result}")
 
 if (__name__ == "__main__"):
     main()
