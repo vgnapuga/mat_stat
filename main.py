ad18b3b2ac94dc7d030f09a797c4fae0484d32f9
OneNull <shavrin_ns@mail.ru>
2025-04-14 14:17:28 +0300

    logical fix

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from typing import Generator
 
 
@@ -5,7 +7,7 @@ def permutations(elements:list, n:int) -> Generator[list, list, list]:
     """Алгоритм Хипа"""
     yield elements.copy()
 
-    c:list = [0] * n
+    c:list[int] = [0] * n
     i = 0
 
     while i < n:
@@ -23,18 +25,22 @@ def permutations(elements:list, n:int) -> Generator[list, list, list]:
             c[i] = 0
             i += 1
 
-def calculate_permutations(n:int) -> list:
-    days:list = [day for day in range(1, n + 1)]
 
-    all_permutations:list = list(permutations(days, n))
+def calculate_permutations(n:int) -> list[int]:
+    days:list[int] = [day for day in range(1, n + 1)]
+
+    all_permutations:list[int] = list(permutations(days, n))
     return all_permutations
 
+
 def solution(n:int, m:int) -> float:
-    permutations:list = calculate_permutations(n)
+    permutations:list[int] = calculate_permutations(n)
     result:int = 0
 
     for permutation in permutations:
-        if (n in permutation[:m]):
+        days_to_wait:list[int] = permutation[:m]
+
+        if (n in days_to_wait):
             continue
 
         last_waiting_day:int = permutation[m - 1]
@@ -45,18 +51,23 @@ def solution(n:int, m:int) -> float:
                 result += 1
                 break
             elif (current_day > last_waiting_day):
+                if (current_day < max(days_to_wait)):
+                    continue
+
                 break
-            else:
-                continue
 
     return result / len(permutations)
 
+
 def main() -> None:
+    os.system("clear")
+
     n:int = int(input("Введите общее кол-во дней: "))
     m:int = int(input("Введите кол-во дней для обязательного ожидания: "))
 
     result:float = solution(n, m)
     print("Вероятность продажи по максимальной цене:", result)
+
 
 if (__name__ == "__main__"):
     main()
